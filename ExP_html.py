50424d7bc130d8d1c6606fe0d25da470159cf9e6
otealexs <alexoteromartin@gmail.com>
2024-12-23 19:34:28 +0000

    input_float hecho

--- ExP_html.py
+++
@@ -1,2 +0,0 @@
-#En el html siempre hay que poner el link al archivo css para que pueda cogerlo.
-#Toda parte que quieras editar con css tienes que hacerle un class="nombre", esto es como si fuera una función en python, es para poder llamarlo cuando queramos en el css.
